ec375376e1740797fd4c9db39d7e205c831a8887
Jesse Naiman <jessenaiman@gmail.com>
2014-02-16 17:26:54 +0000

    fixing admin layout (still blank slate)

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,6 @@
 class DocumentsController < ApplicationController
-  before_filter :authenticate_user!, :except => [:show, :index]
-  before_filter :authenticate_admin!, :only => [:toolkit]
+  before_filter :authenticate_user!, :except => [:show, :index, :toolkit, :toolkit_show]
+  before_filter :authenticate_admin!, :only => [:toolkit, :toolkit_show]
   before_action :set_document, only: [:show, :edit, :update, :destroy]
 
   #respond_to :html, :json
