5a8a25198d4ac27aec1bdd26a6d96a0c8ad76c68
Jesse Naiman <jessenaiman@gmail.com>
2014-03-04 13:34:21 -0500

    ajaxy login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,31 +5,54 @@ class Users::SessionsController < Devise::SessionsController
     #binding.pry
     self.resource = resource_class.new(sign_in_params)
     respond_to do |format|
-      format.js
+      format.js {
+        self.resource = resource_class.new(sign_in_params)
+        clean_up_passwords(resource)
+      }
       format.html { super }
     end
   end
 
 
   def create
-    resource = resource_class.new(sign_in_params)
 
-    sign_in(resource, :bypass => true)
-    #binding.pry
     respond_to do |format|
-      format.js
+      format.js {
+        @user = User.where(email: sign_in_params[:email]).first
+        if @user.nil?
+          @user = User.new
+          @user.errors.add(:email, 'does not exist in our database')
+        else
+          if @user.valid_password?(sign_in_params[:password])
+            sign_in @user
+          else
+            @user.errors.add(:password, 'missing or incorrect please try again')
+          end
+        end
+      }
       format.html { super }
     end
 
   end
 
+  # DELETE /resource/sign_out
   def destroy
-    reset_session
-    redirect_to root_url, :notice => 'Signed out!'
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.js
+      format.html { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
   end
 
   def failure
-    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+    binding.pry
+    #redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
   end
 
 end
