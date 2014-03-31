746842553cc1235301223da5d0b7d072050d2769
media temple admin <webadmin@ontarioliberal.ca>
2014-03-31 18:31:05 -0400

    phil

--- app/controllers/pla_presidents_controller.rb
+++ app/controllers/pla_presidents_controller.rb
@@ -5,12 +5,12 @@ class PlaPresidentsController < ApplicationController
     def index
 
 		@riding_executives = RidingExecutive.where(:role_code => 0)
-											.order_by(:riding_id.desc)
+											.order_by(:riding_id.asc)
 		@riding_executives_hash = Hash.new
 		@riding_executives.each do |riding_executive|
 			@riding_executives_hash[riding_executive.riding_id] = riding_executive
 		end
-		@web_site_managers = WebSiteManager.order_by(:r_id.desc)
+		@web_site_managers = WebSiteManager.order_by(:r_id.asc)
     end
 
 end
